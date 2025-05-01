11756a3ec862bba4696f219ca824001372021997
Petr Skoda <petr@mutms.org>
2025-05-01 06:53:06 +0200

    release mu-4.5.4-09

--- version.php
+++ version.php
@@ -32,7 +32,7 @@ $plugin->version   = 2025042900;
 $plugin->requires  = 2024091700.00;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
-$plugin->release   = 'mu-4.5.4-08';
+$plugin->release   = 'mu-4.5.4-09';
 
 $plugin->dependencies = [
     'tool_mulib' => 2025042900,
