b4446d0a9b919fd473de26051f7318f778c45a12
Petr Skoda <petr@mutms.org>
2025-05-04 13:47:45 +0200

    MuTMS: standardise license notice

--- management/framework_delete.php
+++ management/framework_delete.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Training plugin for Moodle™.
+// This file is part of MuTMS suite of plugins for Moodle™ LMS.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
