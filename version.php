43be6457c85657223f33a54013dbbcdf2c26c022
Petr Skoda <petr@mutms.org>
2025-05-23 17:56:35 +0200

    Use release+ in between releases

--- version.php
+++ version.php
@@ -32,7 +32,7 @@ $plugin->version   = 2025050100;
 $plugin->requires  = 2024091700.00;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
-$plugin->release   = 'mu-4.5.4-10';
+$plugin->release   = 'mu-4.5.4-10+';
 
 $plugin->dependencies = [
     'tool_mulib' => 2025050100,
