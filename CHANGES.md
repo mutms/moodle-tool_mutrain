eb88024103bfb8e12ec35dfe32b0d0ee234b7b3c
Petr Skoda <petr@mutms.org>
2025-06-29 11:42:21 +0200

    Add support for Moodle 5.0

--- CHANGES.md
+++ CHANGES.md
@@ -1,15 +1,7 @@
 # Changelog
 
-## mu-4.5.5-02
+## mu-5.0.1-01
 
 Release date: 30/06/2025
 
-* New plugin versioning.
-
-## mu-4.5.5-01
-
-Release date: 09/06/2025
-
-* Improved docs and added acknowledgements.
-* Fixed ordering of frameworks by name.
-* Standardised admin settings.
+* Added support for Moodle 5.0
--- version.php
+++ version.php
@@ -28,14 +28,14 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutrain';
-$plugin->version = 2025063045;
-$plugin->requires = 2024091700;
+$plugin->version = 2025063050;
+$plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->supported = [405, 405];
-$plugin->incompatible = 500;
-$plugin->release = 'mu-4.5.5-02';
+$plugin->supported = [500, 500];
+// $plugin->incompatible = 501;
+$plugin->release = 'mu-5.0.1-01';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2025063045,
-    'customfield_mutrain' => 2025063045,
+    'tool_mulib' => 2025063050,
+    'customfield_mutrain' => 2025063050,
 ];
