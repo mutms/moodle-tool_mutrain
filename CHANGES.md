8154c0941aefcc1c0a67648ff3bd0c070867f780
Petr Skoda <petr@mutms.org>
2025-06-29 09:31:31 +0200

    Use new plugin versions

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## mu-4.5.5-02
+
+Release date: 30/06/2025
+
+* New plugin versioning.
+
 ## mu-4.5.5-01
 
 Release date: 09/06/2025
--- version.php
+++ version.php
@@ -28,13 +28,14 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutrain';
-$plugin->version   = 2025060900;
-$plugin->requires  = 2024091700.00;
-$plugin->maturity  = MATURITY_ALPHA;
+$plugin->version = 2025063045;
+$plugin->requires = 2024091700;
+$plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
-$plugin->release   = 'mu-4.5.5-01+';
+$plugin->incompatible = 500;
+$plugin->release = 'mu-4.5.5-02';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2025060900,
-    'customfield_mutrain' => 2025060900,
+    'tool_mulib' => 2025063045,
+    'customfield_mutrain' => 2025063045,
 ];
