4fb6f02e46f069c1be400bfa9fb4fcaef27360b7
Petr Skoda <petr@mutms.org>
2025-06-29 10:08:56 +0200

    Fix required version

--- version.php
+++ version.php
@@ -29,7 +29,7 @@ defined('MOODLE_INTERNAL') || die();
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutrain';
 $plugin->version = 2025063045;
-$plugin->requires = 2024091700;
+$plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
