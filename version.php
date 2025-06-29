596074c43c8dec351eb851231fd22c2c2720bef8
Petr Skoda <petr@mutms.org>
2025-06-29 14:30:51 +0200

    Fix annoying CI comment stuff

--- version.php
+++ version.php
@@ -32,7 +32,7 @@ $plugin->version = 2025063050;
 $plugin->requires = 2025041400;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [500, 500];
-// $plugin->incompatible = 501;
+
 $plugin->release = 'mu-5.0.1-01';
 
 $plugin->dependencies = [
