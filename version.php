3373e630550343a074a75d651369169ef0018519
Petr Skoda <petr@mutms.org>
2025-10-06 08:39:38 +0200

    Finalise mu-4.5.7-01 release

--- version.php
+++ version.php
@@ -28,14 +28,14 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutrain';
-$plugin->version = 2025092445;
+$plugin->version = 2025100645;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
-$plugin->release = 'mu-4.5.6-03+';
+$plugin->release = 'mu-4.5.7-01';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2025092445,
-    'customfield_mutrain' => 2025092445,
+    'tool_mulib' => 2025100645,
+    'customfield_mutrain' => 2025100645,
 ];
