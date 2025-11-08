ebfc5cdd4152acaa55d0c3625f0c1e6175c6701a
Petr Skoda <petr@mutms.org>
2025-11-08 09:44:49 +0100

    Prepare mu-4.5.7-02 release

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## mu-4.5.7-02
+
+Release date: 08/11/2025
+
+* No changes.
+
 ## mu-4.5.7-01
 
 Release date: 06/10/2025
--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'tool_mutrain';
 $plugin->version = 2025100645;
 $plugin->requires = 2024100700;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_BETA;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
 $plugin->release = 'mu-4.5.7-01+';
