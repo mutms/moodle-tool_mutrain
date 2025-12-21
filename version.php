f787497bd4bb26e493e24abe1dce3de3bb9961ba
Petr Skoda <petr@mutms.org>
2025-12-21 17:43:11 +0100

    Bump version

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutrain';
-$plugin->version = 2025121645;
+$plugin->version = 2025122145;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [405, 405];
@@ -36,6 +36,6 @@ $plugin->incompatible = 500;
 $plugin->release = 'mu-4.5.8-02+';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2025121645,
-    'customfield_mutrain' => 2025121645,
+    'tool_mulib' => 2025122145,
+    'customfield_mutrain' => 2025122145,
 ];
