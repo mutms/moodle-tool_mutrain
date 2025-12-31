8c8e2c09407a6f6db81647e51608588ddaab42fc
Petr Skoda <petr@mutms.org>
2025-12-31 10:46:35 +0100

    Revert versioning changes

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutrain';
-$plugin->version = '2025123145';
+$plugin->version = 2025123145;
 $plugin->requires = 2024100700;
 $plugin->maturity = MATURITY_BETA;
 $plugin->supported = [405, 405];
@@ -36,6 +36,6 @@ $plugin->incompatible = 500;
 $plugin->release = 'mu-4.5.8-03';
 
 $plugin->dependencies = [
-    'tool_mulib' => '2025123145',
-    'customfield_mutrain' => '2025123145',
+    'tool_mulib' => 2025123145,
+    'customfield_mutrain' => 2025123145,
 ];
