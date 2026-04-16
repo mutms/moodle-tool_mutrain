b0c3b2d8cb39324591b4e9c399b3e3ecff13922f
Petr Skoda <petr@mutms.org>
2026-04-16 09:17:40 +0000

    back to dev

--- version.php
+++ version.php
@@ -32,7 +32,7 @@ $plugin->version = 2026032945;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
-$plugin->release = 'v4.5.10.06';
+$plugin->release = 'v4.5.10.06+';
 
 $plugin->dependencies = [
     'tool_mulib' => 2026032945,
