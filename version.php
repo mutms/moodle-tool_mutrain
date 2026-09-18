37f1128ceed74b7cf21bc636033fda185e21c7c8
Petr Skoda <petr@mutms.org>
2026-09-18 11:49:46 +0200

    Moodle 5.3dev migration

--- version.php
+++ version.php
@@ -28,13 +28,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutrain';
-$plugin->version = 2026091350;
-$plugin->requires = 2025041400;
-$plugin->supported = [500, 502];
+$plugin->version = 2026091953;
+$plugin->requires = 2026091600;
+$plugin->supported = [503, 503];
 
-$plugin->release = 'v5.0.10.01';
+$plugin->release = 'v5.3.0.00';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026091350,
-    'customfield_mutrain' => 2026091350,
+    'tool_mulib' => 2026091953,
+    'customfield_mutrain' => 2026091953,
 ];
