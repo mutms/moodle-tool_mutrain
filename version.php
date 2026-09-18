c3e5ef7b213eba505d0f7e0a29a3b61ae2ea211b
Petr Skoda <petr@mutms.org>
2026-09-18 10:42:32 +0200

    Back to dev

--- version.php
+++ version.php
@@ -28,13 +28,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutrain';
-$plugin->version = 2026091345;
+$plugin->version = 2026091845;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
-$plugin->release = 'v4.5.14.01';
+$plugin->release = 'v4.5.14.01+';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026091345,
-    'customfield_mutrain' => 2026091345,
+    'tool_mulib' => 2026091845,
+    'customfield_mutrain' => 2026091845,
 ];
